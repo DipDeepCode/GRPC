e363d69cc87df1d2558b75faf77fcbfb986cccb0
Sergey Kovalev <dipdeepcode@mail.ru>
2023-08-20 16:21:39 +0300

    Сконфигурирован сервер

---
+++ GRPC_Server/src/main/java/ru/ddc/grpc_server/App.java
@@ -0,0 +1,17 @@
+package ru.ddc.grpc_server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class App {
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080)
+                .addService(new GreetingServiceImpl())
+                .build();
+        server.start();
+        System.out.println("Server started");
+        server.awaitTermination();
+    }
+}
