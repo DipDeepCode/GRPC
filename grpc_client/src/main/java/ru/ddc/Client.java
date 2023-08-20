17a49684793e1956dabcce0fd49f07d6523a5f82
Sergey Kovalev <dipdeepcode@mail.ru>
2023-08-20 17:01:43 +0300

    Реализация клиента

---
+++ grpc_client/src/main/java/ru/ddc/Client.java
@@ -0,0 +1,19 @@
+package ru.ddc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class Client {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+                .usePlaintext()
+                .build();
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+        GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass.HelloRequest
+                .newBuilder().setName("ClientName")
+                .build();
+        GreetingServiceOuterClass.HelloResponse response = stub.greeting(request);
+        System.out.println(response);
+        channel.shutdownNow();
+    }
+}
