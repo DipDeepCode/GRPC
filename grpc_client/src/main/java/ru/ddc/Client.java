5995dd21a39437766a8ac7454cdd5882ce497648
Sergey Kovalev <dipdeepcode@mail.ru>
2023-08-20 17:14:42 +0300

    Клиент изменен на прием потока данных

--- grpc_client/src/main/java/ru/ddc/Client.java
+++ grpc_client/src/main/java/ru/ddc/Client.java
@@ -3,6 +3,8 @@ package ru.ddc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.Iterator;
+
 public class Client {
     public static void main(String[] args) {
         ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
@@ -12,8 +14,10 @@ public class Client {
         GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass.HelloRequest
                 .newBuilder().setName("ClientName")
                 .build();
-        GreetingServiceOuterClass.HelloResponse response = stub.greeting(request);
-        System.out.println(response);
+        Iterator<GreetingServiceOuterClass.HelloResponse> response = stub.greeting(request);
+        while (response.hasNext()) {
+            System.out.println(response.next());
+        }
         channel.shutdownNow();
     }
 }
